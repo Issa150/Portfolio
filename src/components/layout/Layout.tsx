44ebfc2f6e0251a38c53fd4906c6e91e00c9a7a5
Issa JAFARI <ebrahim.tamrin.1@gmail.com>
2025-07-19 10:57:47 +0200

    Redirection debugged + updated cv

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,10 +5,10 @@ import { Suspense } from "react";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen max-w-[1000px] mx-auto grid my-md:grid-cols-[min-content_1fr] grid-cols-1 justify-items-center gap-2">
+    <div className="--min-h-screen max-w-[1000px] mx-auto grid my-md:grid-cols-[min-content_1fr] grid-cols-1 justify-items-center gap-2">
       <NavBar /> {/*this is positioned fixed */}
       <Asider />
-      <main className="my-md:pt-17 my-14:pt-17  my-15:pt-20  w-[95%] sm:w-full lg:w-12/12">
+      <main className="my-md:pt-17 my-14:pt-17  my-15:pt-20  w-[95%] sm:w-full lg:w-full">
         <Suspense fallback={<div className="text-center text-gray-500">Loading...🕓🕟</div>}>
           <Outlet />
         </Suspense>
